aa02dd140e60c212ebc77c553cada6f9862d10d4
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-06-30 20:10:50 +0530

    Create StringVariables.js

---
+++ StringVariables.js
@@ -0,0 +1,6 @@
+//A string variable is just a normal variable that contains a sentence 
+EXAMPLE: 
+var StringVariable = "Hello! This is a string!"
+console.log(StringVariable)
+
+// The double quotes ("") define that it is a string 
