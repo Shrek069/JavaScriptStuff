91b14bba5442a6afe5293491fbb1e722fa4522ef
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-07-10 09:46:32 +0530

    Delete StringVariables.js

--- StringVariables.js
+++
@@ -1,6 +0,0 @@
-//A string variable is just a normal variable that contains a sentence 
-EXAMPLE: 
-var StringVariable = "Hello! This is a string!"
-console.log(StringVariable)
-
-// The double quotes ("") define that it is a string 
