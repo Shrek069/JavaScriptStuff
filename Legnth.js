021a30e16522120058294bd5de024f4c4d0af1ed
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-07-10 10:13:25 +0530

    Create Legnth.js

---
+++ Legnth.js
@@ -0,0 +1,25 @@
+
+
+// The length property returns the length of a string
+
+
+
+
+
+// The length property returns the length of a string
+
+var String = "abcdefghi"
+
+console.log(String.length)
+
+// Or
+
+console.log("abcdefghi".length)
+
+// Or
+
+var String = "abcdefghi"
+
+length = String.length
+
+console.log(length)
